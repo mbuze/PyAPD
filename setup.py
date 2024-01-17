88ff179764ad837e25695f39bfdccb3523280c9e
mbuze <maciej.buze@gmail.com>
2024-01-17 14:47:24 +0000

    change authors

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyAPD',
-    version='0.0.1',
-    author='Maciej Buze',
+    version='0.0.3',
+    author='Maciej Buze, Steve Roper, David Bourne',
     author_email='maciej.buze@gmail.com',
     description='A Python library for generating (optimal) anisotropic power diagrams',
     long_description="""
-        A Python library for computing (optimal) anisotropic power diagrams using GPU acceleration. Current main application concerns geometric modelling of polycrystalline materials with curved boundaries with grains of prescribed volumes and fine control over aspect ratio and location of the grains.
+        A Python library for computing (optimal) anisotropic power diagrams using GPU acceleration.
+        Current main application concerns geometric modelling of polycrystalline materials
+        with curved boundaries with grains of prescribed volumes and fine control over aspect ratio and location of the grains.
     """,
     long_description_content_type="text/markdown",
     url="https://github.com/mbuze/PyAPD",
