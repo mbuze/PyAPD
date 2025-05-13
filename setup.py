3b9d5a333370f5df9dfae8279be456be51706590
mbuze <maciej.buze@gmail.com>
2025-05-13 11:48:53 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='PyAPD',
-    version='0.1.1',
+    version='0.1.2',
     author='Maciej Buze, Steve Roper, David Bourne',
     author_email='maciej.buze@gmail.com',
     description='A Python library for generating (optimal) anisotropic power diagrams',
